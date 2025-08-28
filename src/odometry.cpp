28e607de84d1633084e11d464ac6b209063bfd2b
AnkitS-07 <autobot7nexus@gmail.com>
2025-08-28 18:01:43 +0530

    Update odometry.cpp

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,32 +1,25 @@
-#include "odometry.h"
+#include <iostream>
+#include <vector>
 #include <cmath>
-#include <ctime>
-#include <iterator>
-#include <numeric>
-
 using namespace std;
 
-Odometry::Odometry(double wheel_radius, double rpm)
-    : radius(wheel_radius), rpm(rpm) {
-  // Linear velocity (m/s) =(wheel circumference * revolutions per second)
-  double rps = rpm / 60.0;
-  linear_vel = 2 * M_PI * radius * rps;
-}
+class Odometry {
+public:
+    vector<string> computeCommands(const vector<pair<int,int>>& path) {
+        vector<string> commands;
 
-double Odometry::distance(int x1, int y1, int x2, int y2) {
-  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-}
+        if (path.empty()) return commands;
 
-double Odometry::angle(int x1, int y1, int x2, int y2) {
-  // atan2 returns radians, convert to degrees
-  return atan2(y2 - y1, x2 - x1) * 180.0 / M_PI;
-}
+        for (size_t i = 1; i < path.size(); i++) {
+            int dx = path[i].first - path[i-1].first;
+            int dy = path[i].second - path[i-1].second;
 
-MotionCommand Odometry::computeCommands(vector<pair<int, int>> &path) {
+            if (dx == 1 && dy == 0) commands.push_back("Move Down");
+            else if (dx == -1 && dy == 0) commands.push_back("Move Up");
+            else if (dx == 0 && dy == 1) commands.push_back("Move Right");
+            else if (dx == 0 && dy == -1) commands.push_back("Move Left");
+        }
 
-  MotionCommand res = {0.0, 0.0}; // store total time and angle traversed
-
- /* Implement you odometry logic here */ 
-
-  return res;
-}
+        return commands;
+    }
+};
